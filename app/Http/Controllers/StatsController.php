f009f931a11604676d575471997cea042511a865
Ruben <ruben@one2apps.com>
2017-06-06 16:25:11 +0200

    Add stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -15,6 +15,14 @@ class StatsController extends Controller
     	$data['num_played'] = Game::count();
     	$goals = DB::select("SELECT SUM(score_a) + SUM(score_b) AS total FROM games");
     	$data['num_goals'] = $goals[0]->total;
+    	$data['avg_goals'] = round($goals[0]->total / $data['num_played'], 1);
+
+    	$data['num_users'] = User::count();
+    	$data['goals_per_user'] = round($goals[0]->total / $data['num_users'], 1);
+
+    	$skill = DB::select("SELECT ROUND(AVG(elo_rating), 0) AS avg_rating FROM users");
+    	$data['avg_rating'] = $skill[0]->avg_rating;
+
 
 		return view('stats.index', $data);
     }
--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -2,32 +2,101 @@
 
 @section('content')
 <div class="container">
+
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Statistics</div>
-                <div class="panel-body">
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<div class="row">
-								<div class="col-sm-4">
-									<div class="statistic">
-										<div class="label">Games played</div>
-										<div class="value">{{ $num_played }}</div>
-									</div><!-- /.statistic -->
-								</div><!-- /.col -->
-								<div class="col-sm-4">
-									<div class="statistic">
-										<div class="label">Goals scored</div>
-										<div class="value">{{ $num_goals }}</div>
-									</div><!-- /.statistic -->
-								</div><!-- /.col -->
-							</div><!-- /.row -->
-						</div><!-- /.panel-body -->
-					</div><!-- /.panel -->
-				</div>
+        	<div class="fullWidth eight columns">
+		        <div class="projectFactsWrap blue">
+		            <div class="item wow fadeInUpBig animated animated" style="visibility: visible;">
+		                <i class="fa fa-futbol-o" aria-hidden="true"></i>
+		                <p id="number1" class="number">{{ $num_played }}</p>
+		                <span></span>
+		                <p>Games played</p>
+		            </div>
+		            <div class="item wow fadeInUpBig animated animated" style="visibility: visible;">
+		                <i class="fa fa-bullseye" aria-hidden="true"></i>
+
+		                <p id="number2" class="number">{{ $num_goals }}</p>
+		                <span></span>
+		                <p>Goals scored</p>
+		            </div>
+		            <div class="item wow fadeInUpBig animated animated" style="visibility: visible;">
+		                <i class="fa fa-pie-chart" aria-hidden="true"></i>
+
+		                <p id="number3" class="number">{{ $avg_goals }}</p>
+		                <span></span>
+		                <p>Goals per game</p>
+		            </div>
+		        </div>
+			</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        	<div class="fullWidth eight columns">
+		        <div class="projectFactsWrap red">
+		            <div class="item wow fadeInUpBig animated animated" style="visibility: visible;">
+		                <i class="fa fa-users" aria-hidden="true"></i>
+		                <p id="number4" class="number">{{ $num_users }}</p>
+		                <span></span>
+		                <p>Total users</p>
+		            </div>
+		            <div class="item wow fadeInUpBig animated animated" style="visibility: visible;">
+		               	<i class="fa fa-user-times" aria-hidden="true"></i>
+		                <p id="number5" class="number">{{ $goals_per_user }}</p>
+		                <span></span>
+		                <p>Goals per user</p>
+		            </div>
+		            <div class="item wow fadeInUpBig animated animated" style="visibility: visible;">
+		                <i class="fa fa-trophy" aria-hidden="true"></i>
+
+		                <p id="number6" class="number">{{ $avg_rating }}</p>
+		                <span></span>
+		                <p>Average rating</p>
+		            </div>
+		        </div>
 			</div>
         </div>
     </div>
 </div>
+@endsection
+
+
+@section('extra-scripts')
+<script type="text/javascript">
+
+
+	$.fn.jQuerySimpleCounter = function( options ) {
+	    var settings = $.extend({
+	        start:  0,
+	        end:    100,
+	        easing: 'swing',
+	        duration: 3000,
+	        complete: ''
+	    }, options );
+
+	    var thisElement = $(this);
+
+	    $({count: settings.start}).animate({count: settings.end}, {
+			duration: settings.duration,
+			easing: settings.easing,
+			step: function() {
+				var mathCount = Math.ceil(this.count);
+				thisElement.text(mathCount);
+			},
+			complete: settings.complete
+		});
+	};
+
+
+$('#number1').jQuerySimpleCounter({end: {{ $num_played }}});
+$('#number2').jQuerySimpleCounter({end: {{ $num_goals }}});
+$('#number3').jQuerySimpleCounter({end: {{ $avg_goals }}});
+$('#number4').jQuerySimpleCounter({end: {{ $num_users }}});
+$('#number5').jQuerySimpleCounter({end: {{ $goals_per_user }}});
+$('#number6').jQuerySimpleCounter({end: {{ $avg_rating }}});
+
+
+</script>
+
 @endsection
